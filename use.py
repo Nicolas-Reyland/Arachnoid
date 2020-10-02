4cdba50fcb774815d3448a3010f1783cb60f12aa
= <nicolas@reyland.net>
2020-10-02 13:26:51 +0200

    use.py commit

---
+++ use.py
@@ -0,0 +1,9 @@
+import arachnoid
+from argparse import ArgumentParser
+
+parser = ArgumentParser(description='Quick & basic use of Arachnoid')
+
+parser.add_argument('-r'
+					'--role',
+					type=str)
+
